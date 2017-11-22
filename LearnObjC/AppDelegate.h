3c7a32968e8a3117d9508a07b5121601f63a78fa
Sunny <sunnylee945@hotmail.com>
2017-11-22 09:34:56 +0800

    Initial Commit

---
+++ LearnObjC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LearnObjC
+//
+//  Created by Mac on 2017/11/22.
+//  Copyright © 2017年 Sunny Lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LearnObjC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LearnObjC
+//
+//  Created by Mac on 2017/11/22.
+//  Copyright © 2017年 Sunny Lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
